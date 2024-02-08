150da35ca804497a907087f6f16d038f88e39c94
Zhuoran Zheng <82856113+zzr-idam@users.noreply.github.com>
2024-02-08 14:24:08 +0800

    Update uvmb.py

--- uvmb.py
+++ uvmb.py
@@ -35,18 +35,24 @@ class UVMB(nn.Module):
             expand=2,    # Block expansion factor
         )
         self.smooth = nn.Conv2d(in_channels=c, out_channels=c, kernel_size=3, stride=1, padding=1)
+        self.ln = nn.LayerNorm(normalized_shape=c)
+        self.sm = nn.Softmax(dim=0)
 
     def forward(self, x):
         b,c,w,h = x.shape
         x = self.convb(x) + x
-        x = nn.LayerNorm(normalized_shape=c)(x.reshape(b, -1, c))
+        x = self.ln(x.reshape(b, -1, c))
         y = self.model1(x).permute(0, 2, 1)
         z = self.model3(y).permute(0, 2, 1)
-        att = nn.Softmax()(self.model2(x))
+        att = self.sm(self.model2(x))
         result = att * z
-        output = result.shape(b, c, w, h)
+        output = result.reshape(b, c, w, h)
         return self.smooth(output)
 
 
 
+model = UVMB(c=3,w=64,h=64).to("cuda:0")
 
+data = torch.randn(2, 3, 64, 64).to("cuda:0")
+
+print(model(data).shape)
