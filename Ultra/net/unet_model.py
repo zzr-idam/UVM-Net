957195c1f659dbcd1eb950ce165ba80e4cb36352
Zhuoran Zheng <82856113+zzr-idam@users.noreply.github.com>
2024-03-24 10:18:59 +0800

    Add files via upload

---
+++ Ultra/net/unet_model.py
@@ -0,0 +1,43 @@
+""" Full assembly of the parts to form the complete network """
+
+from unet_part import *
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, 3)
+
+    def forward(self, inp):
+        x = inp
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x) + inp
+        return x
+
+data = torch.randn(2, 3, 128, 128).to("cuda")
+
+model = UNet(n_channels=3).to("cuda")
+
+
+print(model(data).shape)
---
+++ Ultra/net/uvmb.py
@@ -0,0 +1,53 @@
+import torch 
+import torch.nn as nn
+from mamba_ssm import Mamba
+
+
+class UVMB(nn.Module):
+    def __init__(self,c=3,w=256,h=256):
+        super().__init__()
+        self.convb  = nn.Sequential(
+                    nn.Conv2d(in_channels=c, out_channels=16, kernel_size=3, stride=1, padding=1),
+                    nn.ReLU(),
+                    nn.Conv2d(in_channels=16, out_channels=c, kernel_size=3, stride=1, padding=1)
+                        )
+        self.model1 = Mamba(
+    # This module uses roughly 3 * expand * d_model^2 parameters
+            d_model=c, # Model dimension d_model
+            d_state=16,  # SSM state expansion factor
+            d_conv=4,    # Local convolution width
+            expand=2,    # Block expansion factor
+        )
+
+        self.model2 = Mamba(
+            # This module uses roughly 3 * expand * d_model^2 parameters
+            d_model=c, # Model dimension d_model
+            d_state=16,  # SSM state expansion factor
+            d_conv=4,    # Local convolution width
+            expand=2,    # Block expansion factor
+        )
+
+        self.model3 = Mamba(
+            # This module uses roughly 3 * expand * d_model^2 parameters
+            d_model=w*h, # Model dimension d_model
+            d_state=16,  # SSM state expansion factor
+            d_conv=4,    # Local convolution width
+            expand=2,    # Block expansion factor
+        )
+        self.smooth = nn.Conv2d(in_channels=c, out_channels=c, kernel_size=3, stride=1, padding=1)
+        self.ln = nn.LayerNorm(normalized_shape=c)
+        self.softmax = nn.Softmax()
+    def forward(self, x):
+        b,c,w,h = x.shape
+        x = self.convb(x) + x
+        x = self.ln(x.reshape(b, -1, c))
+        y = self.model1(x).permute(0, 2, 1)
+        z = self.model3(y).permute(0, 2, 1)
+        att = self.softmax(self.model2(x))
+        result = att * z
+        output = result.reshape(b, c, w, h)
+        return self.smooth(output)
+
+
+
+
